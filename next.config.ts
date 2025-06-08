1cb89bf13dc58486279e665d3c237ca159a11fe4
Tin Bukovina <tinbukovina1c@gmail.com>
2025-06-08 11:23:01 +0200

    Addes script fro panda css.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -19,7 +18,6 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
-      // You can add other domains here as well
     ],
   },
 };
